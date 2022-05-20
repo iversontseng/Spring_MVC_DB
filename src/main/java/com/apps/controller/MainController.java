2dcc446242e1b3d0bbbfc567c29934593c680564
iversontseng <iversontseng@gmail.com>
2022-05-20 18:49:47 +0800

    first commit

---
+++ src/main/java/com/apps/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.apps.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class MainController {
+   @GetMapping("/home")
+   public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+     model.addAttribute("name", name);
+     return "home";
+   }
+}
